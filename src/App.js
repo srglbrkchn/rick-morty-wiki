1a79f267b25395c73e7e72ec209e07a280c25978
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-21 18:20:26 +0100

    First commit. Set up React environment for Rick and Morty Wiki app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Card from "./components/Card/Card";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="text-center mb-3">Characters</h1>
+            <div className="container">
+                <div className="row">
+                    Filter component will be placed here
+                    <div className="col-lg-8 col-12">
+                        <div className="row">
+                            Card component will be placed here
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
